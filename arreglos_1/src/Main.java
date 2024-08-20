22227bcb77cf60bcd173750ddb2a0ed896f812c2
Claa <ruloclaa@gmail.com>
2024-08-20 11:50:04 -0300

    first commit

---
+++ arreglos_1/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main (String [] args)
+    {
+        int [] enteros = new int[10];
+        enteros[2] = 100;
+        int valor = enteros[2];
+        System.out.printf("%d%n","el valor de la posicion 2 es: ", valor);
+        
+    }
+}
