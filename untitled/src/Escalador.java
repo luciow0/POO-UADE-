ec810186fe123b12cb95e120be3c48054615ed65
luciowo <126731726+luciow0@users.noreply.github.com>
2024-09-12 13:52:39 -0300

    Update Escalador.java

--- untitled/src/Escalador.java
+++ untitled/src/Escalador.java
@@ -2,5 +2,25 @@ public class Escalador extends Ciclista {
     private float aceleracionPromedioDeSubida;
     private float gradoDeRampaSoportada;
 
+    public Escalador(int identificador, String nombreCiclista, int tiempoCarrera, float aceleracionPromedioDeSubida, float gradoDeRampaSoportada) {
+        super(identificador, nombreCiclista, tiempoCarrera);
+        this.aceleracionPromedioDeSubida = aceleracionPromedioDeSubida;
+        this.gradoDeRampaSoportada = gradoDeRampaSoportada;
+    }
 
+    public float getAceleracionPromedioDeSubida() {
+        return aceleracionPromedioDeSubida;
+    }
+
+    public void setAceleracionPromedioDeSubida(float aceleracionPromedioDeSubida) {
+        this.aceleracionPromedioDeSubida = aceleracionPromedioDeSubida;
+    }
+
+    public float getGradoDeRampaSoportada() {
+        return gradoDeRampaSoportada;
+    }
+
+    public void setGradoDeRampaSoportada(float gradoDeRampaSoportada) {
+        this.gradoDeRampaSoportada = gradoDeRampaSoportada;
+    }
 }
