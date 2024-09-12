880564ada664ceff276e7ef12ee259aded9f206a
luciowo <126731726+luciow0@users.noreply.github.com>
2024-09-12 13:50:52 -0300

    Update Ciclista.java

--- untitled/src/Ciclista.java
+++ untitled/src/Ciclista.java
@@ -4,11 +4,36 @@ public abstract class Ciclista {
     private int tiempoCarrera;
 
     protected String imprimirTipo() {
-        return "Ciclista{" +
-                "identificador=" + identificador +
-                ", nombreCiclista='" + nombreCiclista + '\'' +
-                ", tiempoCarrera=" + tiempoCarrera +
-                '}';
+        return "Es un " + getClass();
+    }
 
+    public Ciclista(int identificador, String nombreCiclista, int tiempoCarrera){
+        this.identificador  = identificador;
+        this.nombreCiclista = nombreCiclista;
+        this.tiempoCarrera = tiempoCarrera;
+    }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(int identificador) {
+        this.identificador = identificador;
+    }
+
+    public String getNombreCiclista() {
+        return nombreCiclista;
+    }
+
+    public void setNombreCiclista(String nombreCiclista) {
+        this.nombreCiclista = nombreCiclista;
+    }
+
+    public int getTiempoCarrera() {
+        return tiempoCarrera;
+    }
+
+    public void setTiempoCarrera(int tiempoCarrera) {
+        this.tiempoCarrera = tiempoCarrera;
     }
 }
