7c586b49af12ddbcf7943a01114dfd228b7c7bed
luciowo <126731726+luciow0@users.noreply.github.com>
2024-09-12 13:51:49 -0300

    Update Velocista.java

--- untitled/src/Velocista.java
+++ untitled/src/Velocista.java
@@ -4,4 +4,25 @@ public class Velocista extends Ciclista {
 
     private double velocidadPromedio;
 
+    public Velocista(int identificador, String nombreCiclista, int tiempoCarrera, double potenciaPromedio, double velocidadPromedio) {
+        super(identificador, nombreCiclista, tiempoCarrera);
+        this.potenciaPromedio = potenciaPromedio;
+        this.velocidadPromedio = velocidadPromedio;
+    }
+
+    public double getPotenciaPromedio() {
+        return potenciaPromedio;
+    }
+
+    public void setPotenciaPromedio(double potenciaPromedio) {
+        this.potenciaPromedio = potenciaPromedio;
+    }
+
+    public double getVelocidadPromedio() {
+        return velocidadPromedio;
+    }
+
+    public void setVelocidadPromedio(double velocidadPromedio) {
+        this.velocidadPromedio = velocidadPromedio;
+    }
 }
