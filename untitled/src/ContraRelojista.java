c9a30a4f2e62d57217036cbc105dcd73398ae59a
luciowo <126731726+luciow0@users.noreply.github.com>
2024-09-12 13:52:15 -0300

    Update ContraRelojista.java

--- untitled/src/ContraRelojista.java
+++ untitled/src/ContraRelojista.java
@@ -1,6 +1,16 @@
 public class ContraRelojista extends Ciclista {
     private int velocidadMaxima;
 
+    public ContraRelojista(int identificador, String nombreCiclista, int tiempoCarrera, int velocidadMaxima) {
+        super(identificador, nombreCiclista, tiempoCarrera);
+        this.velocidadMaxima = velocidadMaxima;
+    }
 
+    public int getVelocidadMaxima() {
+        return velocidadMaxima;
+    }
 
+    public void setVelocidadMaxima(int velocidadMaxima) {
+        this.velocidadMaxima = velocidadMaxima;
+    }
 }
