cf9903737e93a73398f93216799430a825fd9f0c
luciowo <126731726+luciow0@users.noreply.github.com>
2024-09-19 14:02:54 -0300

    Update Main.java

--- cuentaBancaria/src/Main.java
+++ cuentaBancaria/src/Main.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
 
+        Scanner scanner = new Scanner(System.in);
         cuentaBancaria cuenta = new cuentaBancaria();
 
         // ingreso del nombre
@@ -23,28 +23,51 @@ public class Main {
         int numeroCuenta = scanner.nextInt();
         cuenta.setNumeroDeCuenta(numeroCuenta);
 
-        //ingreso del tipo de cuenta
-        System.out.println("ingrese su tipo de cuenta (cuenta de ahorros o cuenta corriente) ");
-        String tipoCuenta = scanner.next();
-
+        // ingreso del tipo de cuenta
+        // establezco tipo cuenta en null para que entre directamente en el bucle y validar
+        String tipoCuenta = null;
         // Validación del tipo de cuenta
-        while (!tipoCuenta.equals("cuenta de ahorros") && !tipoCuenta.equals("cuenta corriente")) {
+        while (tipoCuenta == null || (!tipoCuenta.equals("cuenta de ahorros") && !tipoCuenta.equals("cuenta corriente"))) {
             System.out.println("Ingrese un tipo de cuenta válido (cuenta de ahorros o cuenta corriente): ");
-            tipoCuenta = scanner.next();
+            tipoCuenta = scanner.nextLine();
+            cuenta.setTipoDeCuenta(tipoCuenta);
         }
 
         // ingreso saldo de cuenta
-        System.out.println("ingrese el nombre de su cuenta bancaria ");
+        System.out.println("ingrese el saldo de su cuenta bancaria ");
         int saldo = scanner.nextInt();
         cuenta.setSaldoDeCuenta(saldo);
 
-
-
+        // mostrar datos por pantalla
         System.out.println(cuenta.getNombreDelTitular());
         System.out.println(cuenta.getApellidosDelTitular());
         System.out.println(cuenta.getNumeroDeCuenta());
         System.out.println(cuenta.getTipoDeCuenta());
         System.out.println(cuenta.getSaldoDeCuenta());
+
+        // consulta del saldo de cuenta actualizado
+        System.out.println("Desea consultar el saldo de su cuenta? responda 'true' para si, 'false' para no ");
+        boolean consultarSaldo = scanner.nextBoolean();
+        if(consultarSaldo == true)
+            System.out.println(cuenta.getSaldoDeCuenta());
+        else
+            System.out.println("ok");
+
+        // actualizar saldo de cuenta
+        System.out.println("Desea actualizar el saldo de su cuenta? ingrese el valor actualizado ");
+        int saldoActualizado = scanner.nextInt();
+        System.out.println("Su saldo ahora es: ");
+        System.out.println(cuenta.actualizarSaldo(saldoActualizado));
+
+        // retiro de dinero
+        System.out.println("ingrese la cantidad de dinero que desea retirar de la cuenta ");
+        int dineroRetirado = scanner.nextInt();
+        System.out.println("retirando de su cuenta : " + dineroRetirado);
+        saldoActualizado = cuenta.retirarSaldo(dineroRetirado);
+        System.out.println("su saldo ahora es " + saldoActualizado);
+
+
+
     }
 
 }
